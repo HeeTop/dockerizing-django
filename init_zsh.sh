44962fbf54f204c0a172cd35fdca73f3d9d3da92
heetop <wjdgmlxkr@gmail.com>
2020-03-14 23:27:46 +0900

    doc: init zsh setting shell script

--- init_zsh.sh
+++ init_zsh.sh
@@ -6,10 +6,10 @@ sudo apt-get -y install vim
 
 # install zsh
 sudo apt-get -y install zsh
-sudo chsh -s $(which zsh) && echo "2" | zsh
+sudo chsh -s $(which zsh) && zsh
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
 
 # source ~/.zshrc
 
@@ -17,7 +17,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # auto jump
-# git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump && cd ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/autojump && python3 install.py&& cd
+# git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump && cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump && python3 install.py&& cd
 
 # syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -28,7 +28,7 @@ echo "source ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighti
 echo "plugins=(git zsh-autosuggestions)" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # theme
-echo "ZSH_THEME="\"agnoster\"" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "ZSH_THEME"="\"agnoster\"" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # restart shell
 source ${ZDOTDIR:-$HOME}/.zshrc
